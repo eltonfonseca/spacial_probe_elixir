16e2d28fc0c07e0f50ed90ac3fd0f798d3121c86
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2022-12-23 18:33:20 -0300

    add default test

--- test/spacial_probe_web/controllers/page_controller_test.exs
+++ test/spacial_probe_web/controllers/page_controller_test.exs
@@ -1,8 +1,17 @@
 defmodule SpacialProbeWeb.PageControllerTest do
   use SpacialProbeWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  describe "POST #move_probe" do
+    test "returns default position when moviments is empty", %{conn: conn} do
+      coords = %{moviments: []}
+      expected = %{"x" => 0, "y" => 0, "face" => "Right"}
+
+      response =
+        conn
+        |> post("/move_probe", coords)
+        |> json_response(201)
+
+      assert response = expected
+    end
   end
 end
