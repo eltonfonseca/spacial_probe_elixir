8e12ee5f03ce6b452dcbca756e5a1e8b2c97d1e8
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2023-01-20 16:25:54 -0300

    fix initial test

--- test/spacial_probe_web/controllers/page_controller_test.exs
+++ test/spacial_probe_web/controllers/page_controller_test.exs
@@ -1,17 +1,17 @@
 defmodule SpacialProbeWeb.PageControllerTest do
   use SpacialProbeWeb.ConnCase
 
-  describe "POST #move_probe" do
+  describe "POST #move" do
     test "returns default position when moviments is empty", %{conn: conn} do
       coords = %{moviments: []}
       expected = %{"x" => 0, "y" => 0, "face" => "Right"}
 
       response =
         conn
-        |> post("/move_probe", coords)
-        |> json_response(201)
+        |> post("/api/move", coords)
+        |> json_response(200)
 
-      assert response = expected
+      assert response == expected
     end
   end
 end
