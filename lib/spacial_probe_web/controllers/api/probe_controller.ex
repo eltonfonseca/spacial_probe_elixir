783cd3f6399aaab2aa1ca77170fdfeefd40400b9
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2023-03-17 18:30:57 -0300

    fix controller

--- lib/spacial_probe_web/controllers/api/probe_controller.ex
+++ lib/spacial_probe_web/controllers/api/probe_controller.ex
@@ -1,9 +1,18 @@
 defmodule SpacialProbeWeb.Api.ProbeController do
   use SpacialProbeWeb, :controller
 
-  alias SpacialProbe.Probe
+  alias SpacialProbeApi.Services.Probe, as: ProbeService
 
   def move(conn, params) do
-    render(conn, "probe_status.json", probe: %Probe{})
+    case ProbeService.move(params) do
+      {:ok, probe} -> render(conn, "probe_status.json", probe: probe)
+      {:error, message} -> render_error(conn, message)
+    end
+  end
+
+  defp render_error(conn, message) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> json(message)
   end
 end
