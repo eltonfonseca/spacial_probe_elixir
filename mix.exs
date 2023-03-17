11c0f15506e09c42d1cdec0d2bde37415405a555
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2023-03-17 18:33:45 -0300

    add credo

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule SpacialProbe.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
